4d3985ddea8b2d090c48d787eb3906e47ee33453
megtsuru <39535180+megtsuru@users.noreply.github.com>
2018-05-22 21:40:45 -0300

    Atributos e metodos adicionados baseando-se no exercicio 2

--- lista07.Isabella.Megumi.Q/src/lista07/Isabella/Megumi/Q/Jedi.java
+++ lista07.Isabella.Megumi.Q/src/lista07/Isabella/Megumi/Q/Jedi.java
@@ -2,11 +2,10 @@ package lista07.Isabella.Megumi.Q;
 
 public class Jedi extends Ordem {
 	private double percentualPazInterna;
-
+	
 	public double getPercentualPazInterna() {
 		return percentualPazInterna;
 	}
-
 	public void setPercentualPazInterna(double percentualPazInterna) {
 		this.percentualPazInterna = percentualPazInterna;
 	}
--- lista07.Isabella.Megumi.Q/src/lista07/Isabella/Megumi/Q/Mestre.java
+++ lista07.Isabella.Megumi.Q/src/lista07/Isabella/Megumi/Q/Mestre.java
@@ -3,7 +3,21 @@ package lista07.Isabella.Megumi.Q;
 public class Mestre extends Cavaleiros {
 	private int campoVidencia;
 	private boolean imortalidade;
-	
+	private int quantidadeVida = 100;
+	private String dominioSabre;
+
+	public String getDominioSabre() {
+		return dominioSabre;
+	}
+	public void setDominioSabre(String dominioSabre) {
+		this.dominioSabre = dominioSabre;
+	}
+	public int getQuantidadeVida() {
+		return quantidadeVida;
+	}
+	public void setQuantidadeVida(int quantidadeVida) {
+		this.quantidadeVida = quantidadeVida;
+	}
 	public int getCampoVidencia() {
 		return campoVidencia;
 	}
