e6cf02e55f83e8a94f2ba4a10531e8563de7fbf3
megtsuru <39535180+megtsuru@users.noreply.github.com>
2018-05-28 15:11:11 -0300

    Adicao de comentário

--- lista07.Isabella.Megumi.Q/src/lista07/Isabella/Megumi/Q/Batalha.java
+++ lista07.Isabella.Megumi.Q/src/lista07/Isabella/Megumi/Q/Batalha.java
@@ -1,11 +1,60 @@
 package lista07.Isabella.Megumi.Q;
 
+/*
+ * O programa é um jogo interativo que simula a batalha entre um Mestre Jedi e um Lorde Sith.
+ * 
+ * Inicialmente, o programa pede para que se digite o nome, o valor de dominio da forca(numeros inteiros de 1 a 10) 
+ * e o valor de dominio do sabre (numeros inteiros de 1 a 10) de cada um dos personagens.
+ * 
+ * 
+ * Então, é iniciada a batalha em que em cada jogada:
+ * 		 i.   Eh escolhida uma skill(habilidade) para cada personagem,
+ * 		 ii.  Cada personagem sofre ou nao um dano (a quantidade de vida eh subtraida) dependendo das skills escolhidas, e
+ *	     iii. O jogo encerra quando um dos personagens fica com a quantidade de vida menor ou igual a zero, sendo este o perdedor.
+ * 
+ * Detalhando as regras do jogo:
+ * 
+ *  a) Cada um dos personagens iniciam com quantidade de vida igual a 100.
+ *  
+ * 	b) As skills (habilidades) sao as mesmas para os dois personagens. Em cada jogada, o programa pede para que se
+ *     escolha uma das skills entre as que são listadas, uma para o Mestre Jedi e outra para o Lorde Sith, sendo elas:
+ * 			0. atacar com sabre 		(12)
+ * 			1. atacar com dupla forca 	(10)
+ * 			2. atacar com forca 		(8)
+ * 			3. esquivar					(0)
+ *	   Os numeros entre parenteses sao o dano minimo (quantidade minima de vida que a skill retira do oponente) associado 
+ *	   a cada skill.
+ *	   Além disso, 
+ *	   		(0) eh uma skill do tipo habilidade com uso de sabre;
+ *	   		(1) e (2) sao skills do tipo habilidade com uso de forca;
+ *			(3) eh uma skill do tipo habilidade de esquiva.
+ *
+ * 	c) Cada skill tem um dano minimo associado a ela, porem o dano real da skill depende do dominio(forca ou sabre) 
+ * 	   associado a cada personagem. Assim, o dano real eh calculado atraves da formula abaixo:
+ * 			Dano real = dano minimo * (1 + 0,1 * (dominio))
+ * 	   Sendo dominio 
+ * 			i.  o valor do dominio da forca se a skill eh do tipo habilidade com uso de forca;
+ * 			ii. o valor do dominio do sabre se a skill eh do tipo habilidade com uso de sabre.
+ * 
+ *  d) A skill esquivar evita que o personagem sofra dano no caso em que o oponente tenha escolhido um ataque com sabre.
+ * 
+ *  e) Cada skill tem uma prioridade de execucao sobre a outra, sendo esta, listada na ordem crescente:
+ *			atacar com sabre < atacar com dupla forca < atacar com forca < esquivar
+ *	
+ *  
+ *  f) Caso tenha sido escolhida a mesma skill para os dois jogadores, temos duas possibilidades de saida:
+ *  		i.  Tem prioridade de execucao o personagem que tenha o maior dominio associado ao tipo da skill escolhida.
+ *  		ii. Caso os personagens tenham o mesmo dominio associado ao tipo da skill escolhida, a prioridade de execucao
+ *  			eh definida por uso da aleatoriedade.
+ */
+
+
 import java.util.Scanner;
 import java.util.Random;
 
 public class Batalha {
-	private static final String[] skills = {"atacar com sabre", "atacar com dupla forca", "atacar com forca", "esquivar"};
-	private static final int[] dano = {12, 10, 8, 0};
+	private static final String[] skills = {"atacar com sabre", "atacar com dupla forca", "atacar com forca", "esquivar"}; //lista de skills (habilidades)
+	private static final int[] dano = {12, 10, 8, 0}; //indica a quantidade de vida que uma skill (habilidade) tira do oponente
 	private static String nome;
 	private static int dominioForca, dominioSabre;
 	private Scanner keyboard = new Scanner(System.in);
@@ -46,6 +95,7 @@ public class Batalha {
 		Lorde lordeSith = new Lorde(nome, dominioForca, dominioSabre);
 		System.out.println("\nNome: " + lordeSith.getNome() + "\nDominio da forca: " + lordeSith.getDominioForca() + "\nDominio do Sabre: " + lordeSith.getDominioSabre() + "\n");
 
+		//saída do loop quando um dos personagens fica com a vida zerada
 		while (true) {
 			System.out.println("----------- " + cont + " -----------");
 			
@@ -58,8 +108,8 @@ public class Batalha {
 			skillLorde = keyboard.nextInt();
 			keyboard.nextLine();
 			
+			//caso em que os jogadores escolhem a mesma habilidade (skill)		
 			if (skillMestre == skillLorde) {
-				//atencao: caso mesmo skill mesmo nivel -> usemos sorteio
 				int skill = skillMestre;
 				if (skill == 3) {
 					System.out.println("Os dois esquivaram!");
@@ -118,6 +168,7 @@ public class Batalha {
 						}
 					}
 				}
+				//caso em que jogadores escolhem habilidades diferentes
 				else {
 					if (mestreJedi.getDominioSabre() > lordeSith.getDominioSabre()) {
 						lordeSith.setVidaPosDano(dano[skill]*(1 + 0.1 * lordeSith.getDominioSabre()));
